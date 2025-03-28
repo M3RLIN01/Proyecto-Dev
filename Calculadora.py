8560909c78c3c3db05822d4ab39c300dd716c489
M3RLIN01 <changeme@example.com>
2025-03-28 15:51:09 +0000

    Agregamos, resta, multiplicacion y division

--- Calculadora.py
+++ Calculadora.py
@@ -4,3 +4,6 @@ num1 = int(input("Ingrese el primer Número: "))
 num2 = int(input("Ingrese el segundo Número: "))
 
 print(f"La suma de los dos valores es: {num1 + num2}")
+print(f"La resta de los dos valores es: {num1 - num2}")
+print(f"La Multiplicaion de los dos valores es: {num1 * num2}")
+print(f"La Division de los dos valores es: {num1 // num2}")
