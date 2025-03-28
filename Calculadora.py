e8015ac3f4a0a7ea61b93d1eb729dafec07ba983
M3RLIN01 <changeme@example.com>
2025-03-28 15:55:32 +0000

    Correccion de gramatica

--- Calculadora.py
+++ Calculadora.py
@@ -3,4 +3,7 @@ print("Welcome stranger")
 num1 = int(input("Ingrese el primer Número: "))
 num2 = int(input("Ingrese el segundo Número: "))
 
-print(f"La suma de los dos valores es: {num1 + num2}")
+print(f"La Suma de los dos valores es: {num1 + num2}")
+print(f"La Resta de los dos valores es: {num1 - num2}")
+print(f"La Multiplicaion de los dos valores es: {num1 * num2}")
+print(f"La Division de los dos valores es: {num1 // num2}")
